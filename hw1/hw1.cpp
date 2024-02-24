70b1e1be18ca93360ce3285d1c6e9277d70cee25
HideBa <baba.papa1120.ba@gmail.com>
2024-02-24 21:25:48 +0100

    wip: imple plane

--- hw1/hw1.cpp
+++ hw1/hw1.cpp
@@ -9,17 +9,18 @@
 #include <CGAL/Triangulation_vertex_base_2.h>
 #include <CGAL/linear_least_squares_fitting_3.h>
 
-// typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
-typedef CGAL::Simple_cartesian<double> Kernel;
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef CGAL::Exact_predicates_tag Tag;
 
 struct FaceInfo {
   bool interior, processed;
+
   FaceInfo() {
     processed = false;
     interior = false;
   }
 };
+
 typedef CGAL::Triangulation_vertex_base_2<Kernel> VertexBase;
 typedef CGAL::Constrained_triangulation_face_base_2<Kernel> FaceBase;
 typedef CGAL::Triangulation_face_base_with_info_2<FaceInfo, Kernel, FaceBase>
@@ -30,13 +31,18 @@ typedef CGAL::Constrained_Delaunay_triangulation_2<
     Kernel, TriangulationDataStructure, Tag>
     Triangulation;
 
-const std::string input_file = "./data/NL.IMBAG.Pand.0503100000000010-0.obj";
-const std::string output_file = "./out/test.obj";
+const std::string input_file =
+    "/Users/hideba/private-proj/tudelft/third-q/3dmodelling/"
+    "assignments-3dmodelling/hw1/data/NL.IMBAG.Pand.0503100000000010-0.obj";
+const std::string output_file =
+    "/Users/hideba/private-proj/tudelft/third-q/3dmodelling/"
+    "assignments-3dmodelling/hw1/out/test.obj";
 
 // TODO: delete me later
-typedef Kernel::Point_3 Point;
-typedef Kernel::Plane_3 Plane;
-typedef CGAL::Simple_cartesian<double>::Triangle_3 Triangle;
+typedef Kernel::Point_3 Point3;
+typedef Kernel::Point_2 Point2;
+typedef Kernel::Triangle_3 Triangle3;
+typedef Kernel::Plane_3 Plane3;
 
 struct Vertex {
   double x, y, z;
@@ -108,35 +114,33 @@ int main(int argc, const char *argv[]) {
   //   std::cout << std::endl;
   // }
 
-  for (auto const &face : faces) {
-    std::cout << "best fitting__2222oo____" << std::endl;
+  for (auto &face : faces) {
     std::vector<Kernel::Point_3> points;
-
     for (int index : face.boundary) {
       const Vertex &v = vertices[index];
-      points.push_back(Kernel::Point_3(v.x, v.y, v.z));
+      Point3 point = Point3(v.x, v.y, v.z);
+      points.push_back(point);
     }
 
     if (!points.empty()) {
-      Plane plane;
-      CGAL::linear_least_squares_fitting_3(points.begin(), points.end(), plane,
-                                           CGAL::Dimension_tag<2>());
+      CGAL::linear_least_squares_fitting_3(points.begin(), points.end(),
+                                           face.best_plane,
+                                           CGAL::Dimension_tag<0>());
     }
   }
 
   // Triangulate faces (to do)
   // Project to 2D plane
-  // for (auto const &face : faces) {
-  //   std::cout << "best fitting!!!!!!!" << std::endl;
-  //   for (auto const &vertex_index : face.boundary) {
-  //     CGAL::Point_2<Kernel> projected_vertex;
-  //     projected_vertex = face.best_plane.to_2d(CGAL::Point_3<Kernel>(
-  //         vertices[vertex_index].x, vertices[vertex_index].y,
-  //         vertices[vertex_index].z));
-  //   }
-  //   std::cout << "best plane" << i++ << ": " << face.best_plane;
-  //   std::cout << std::endl;
-  // }
+  for (auto &face : faces) {
+    for (auto const &vertex_index : face.boundary) {
+      Point2 projected_vertex = face.best_plane.to_2d(
+          Point3(vertices[vertex_index].x, vertices[vertex_index].y,
+                 vertices[vertex_index].z));
+    }
+    std::cout << "best plane" << i++ << ": " << face.best_plane;
+    std::cout << std::endl;
+  }
+
   // Label triangulation (to do)
 
   // Export triangles (to do)

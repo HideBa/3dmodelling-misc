a51ffee81613060c83e1a5542782d5c151210ebe
HideBa <baba.papa1120.ba@gmail.com>
2024-03-21 14:32:15 +0100

    feat: make lod1.2 multi-surface

--- hw2/cpp/src/main.cpp
+++ hw2/cpp/src/main.cpp
@@ -48,6 +48,10 @@ json lod1_2(json &j);
 
 json extract_lod1_2(json &j);
 
+vec<double> transform(vec<int> vertices, vec<double> scales, vec<double> translates);
+
+vec<int> rev_transform(vec<double> vertices, vec<double> scales, vec<double> translates);
+
 std::vector<std::string> BUILDING_TYPE = {"Building", "BuildingPart",
                                           "BuildingRoom", "BuildingStorey",
                                           "BuildingUnit"};
@@ -79,12 +83,12 @@ int main(int argc, const char *argv[]) {
 
     json lod0_2_json = lod0_2(j);
 
-//    json outJson = extract_lod1_2(lod0_2_json);
+    json outJson = extract_lod1_2(lod0_2_json);
 
     //-- write to disk the modified city model (out.city.json)
     std::ofstream o(outFineName);
-//    o << outJson.dump(2) << std::endl;
-    o << lod0_2_json.dump(2) << std::endl;
+    o << outJson.dump(2) << std::endl;
+//    o << lod0_2_json.dump(2) << std::endl;
     o.close();
     std::cout << "file written" << std::endl;
     return 0;
@@ -157,18 +161,22 @@ json lod0_2(json &js) {
 json extract_lod1_2(json &js) {
     const vec<double> scales = js["transform"]["scale"];
     const vec<double> translates = js["transform"]["translate"];
-    cout << "run-----------------" << endl;
     for (auto &co: js["CityObjects"].items()) {
         if (std::find(BUILDING_TYPE.begin(), BUILDING_TYPE.end(),
                       co.value()["type"]) != BUILDING_TYPE.end()) {
 
             //            extract lod0.2
+            json lod1_2_geometry = {{"lod",  "1.2"},
+                                    {"type", "MultiSurface"}};
+            vec<vec<vec<int>>> lod1_2_boundaries;
             for (auto &g: co.value()["geometry"]) {
+                double roof_height;
                 if (g["lod"] == "2.2") {
                     for (auto &shell: g["boundaries"]) {
+                        // Extract lod0.2
                         vec<vec<vec<int>>> lod0_2_boundaries;
+                        pair<int, double> lower_surface;
                         for (size_t i = 0; i < shell.size(); i++) {
-                            pair<int, double> lower_surface;
                             json surface = shell[i];
                             vec<vec<int>> surface_boundary;
                             for (size_t j = 0; j < surface.size(); j++) {
@@ -186,20 +194,12 @@ json extract_lod1_2(json &js) {
                                     lower_surface = {j, average_z};
                                 }
                             }
-                            vec<vec<int>> ground_surface = shell[lower_surface.first];
-                            lod0_2_boundaries.push_back(ground_surface);
                         }
-
-                        json lod1_0_geometry = {{"lod",  "0.7"},
-                                                {"type", "MultiSurface"}};
-                        vec<vec<vec<int>>> lod1_0_boundaries;
-                        vec<vec<vec<int>>> roof_multi_surface;
-                        double roof_height;
+                        lod0_2_boundaries.push_back(shell[lower_surface.first]);
+                        // Decide the height of the roof
                         for (size_t i = 0; i < shell.size(); i++) {
                             json surface = shell[i];
-
                             vec<vec<int>> roof_surface_boundary;
-                            vec<vec<vec<int>>> wall_surfaces_boundary;
                             for (size_t j = 0; j < surface.size(); j++) {
                                 vec<vec<double>> vertices;
                                 vec<Point3> points;
@@ -240,91 +240,89 @@ json extract_lod1_2(json &js) {
                                     roof_height = lod1_2_roof_height;
                                 }
                             }
-                            roof_multi_surface.push_back(roof_surface_boundary);
                         }
 
-                        // print out shape of lod0_2_boundaries
-//                        for (auto &surface: lod0_2_boundaries) {
-//                            for (auto &boundary: surface) {
-//                                for (auto &vertex: boundary) {
-//                                    cout << vertex << " ";
-//                                }
-//                                cout << endl;
-//                            }
-//                        }
+                        vec<pair<int, int>> roof_ground_pairs;
+                        // Add roof vertices to vertices list
+                        vec<vec<vec<int>>> roof_multi_surface;
+                        for (auto &surface: lod0_2_boundaries) {
+                            vec<vec<int>> roof_surface;
+                            for (auto &boundary: surface) {
+                                vec<int> inner_roof;
+                                for (auto &v: boundary) {
+                                    vec<int> vertex = js["vertices"][v];
+                                    vec<double> tv = transform(vertex, scales, translates);
+
+                                    vec<double> roof_v = {tv[0], tv[1], tv[2] + roof_height};
+                                    vec<int> rev_vertex = rev_transform(roof_v, scales, translates);
+                                    js["vertices"].push_back(rev_vertex);
+                                    inner_roof.push_back(js["vertices"].size() - 1);
+                                    roof_ground_pairs.push_back({v, js["vertices"].size() - 1});
+                                }
+
+                                //reverse the orientation of the inner array
+                                reverse(inner_roof.begin(), inner_roof.end());
+                                roof_surface.push_back(inner_roof);
+                            }
+                            roof_multi_surface.push_back(roof_surface);
+                        }
+
+
+
+//                        MEMO: this is for debug
+//                        ========================
+                        json lod1_0_geometry = {{"lod",  "1.0"},
+                                                {"type", "MultiSurface"}};
+                        lod1_0_geometry["boundaries"] = roof_multi_surface;
+                        co.value()["geometry"].push_back(lod1_0_geometry);
+//                        ========================
+
 
                         vec<vec<vec<int>>> multi_ground_surface = lod0_2_boundaries;
+                        vec<vec<vec<int>>> lod1_2_boundaries;
                         vec<vec<vec<int>>> wallsurfaces;
-                        // loop  boundaries with pair of vertices and construct wall surface which consists of 4 vertices 2 for ground surface and 2 for roof surface)
-//                                TODO: DELETE ME
-                        if (roof_multi_surface.size() != multi_ground_surface.size()) {
-                            cout << "multi ground surface size: " << multi_ground_surface.size() << endl;
-                            cout << "roof multi surface size: " << roof_multi_surface.size() << endl;
-                            cout << "warining!!!!????? different size!!! multi x multi" << endl;
-                        }
+
                         for (size_t i = 0; i < multi_ground_surface.size(); i++) {
                             vec<vec<int>> ground_surface = multi_ground_surface[i];
-                            vec<vec<int>> roof_surface = roof_multi_surface[i];
-                            vec<vec<int>> wall_surface;
-                            if (ground_surface.size() != roof_multi_surface[i].size()) {
-                                cout << "ground surface size: " << ground_surface.size() << endl;
-                                cout << "roof surface size: " << roof_surface.size() << endl;
-                                cout << "warining!!!!????? different size!!! ground surface x roof surface" << endl;
-                            }
-                            for (size_t j = 0; i < ground_surface.size(); j++) {
+                            for (size_t j = 0; j < ground_surface.size(); j++) {
                                 vec<int> inner_ground_surface = ground_surface[j];
-                                vec<int> inner_roof_surface = roof_surface[j];
                                 bool is_outer = j == 0;
-                                if (inner_ground_surface.size() != inner_roof_surface.size()) {
-                                    cout << "warining!!!! different size!!! inner!!!" << endl;
-                                }
-                                vec<int> inner_wall_surface;
-                                for (size_t k = 0; k < inner_ground_surface.size(); k++) {
-                                    int ground_vertex_1 = inner_ground_surface[k];
-                                    int ground_vertex_2 = inner_ground_surface[(k - 1) %
-                                                                               inner_ground_surface.size()]; // This is because it should be CCW. In case when it's inner boundary, the orientation should be oppsoite
-                                    int roof_vertex_1 = inner_roof_surface[(k - 1) % inner_ground_surface.size()];
-                                    int roof_vertex_2 = inner_roof_surface[k];
-                                    inner_wall_surface.push_back(ground_vertex_1);
-                                    inner_wall_surface.push_back(ground_vertex_2);
-                                    inner_wall_surface.push_back(roof_vertex_2);
-                                    inner_wall_surface.push_back(roof_vertex_1);
-                                }
-                                wall_surface.push_back(inner_wall_surface);
-                            }
-                            lod1_0_boundaries.push_back(wall_surface);
-                        }
-//                        lod1_0_boundaries.push_back(wall);
-                    }
 
-//                    json lod1_2_geometry = {{"lod",  "1.2"},
-//                                            {"type", "MultiSurface"}};
-//                    vec<vec<vec<vec<int>>>> lod1_2_boundaries;
-//                    vec<vec<vec<int>>> roof_multi_surface;
-//                    for (auto &surface: lod0_2_boundaries) {
-//                        vec<vec<int>> roof_surface;
-//                        for (size_t i = 0; i < surface.size(); i++) {
-//                            vec<vec<double>> vertices;
-//                            vec<Point3> points;
-//                            vec<int> roof;
-//                            for (auto &boundary: surface[i]) {
-//                                vec<int> vertex = js["vertices"][boundary];
-//                                vec<double> tv = transform(vertex, scales, translates);
-//                                Point3 point = Point3(tv[0], tv[1], tv[2]);
-//                                vertices.push_back(tv);
-//                                points.push_back(point);
-//
-//                                vec<double> roof_v = {tv[0], tv[1], tv[2] + roof_height};
-//                                vec<int> rev_vertex = rev_transform(roof_v, scales, translates);
-//                                js["vertices"].push_back(rev_vertex);
-//                                roof.push_back(js["vertices"].size() - 1);
-//                            };
-//                            roof_surface.push_back(roof);
-//                        }
-//                        roof_multi_surface.push_back(roof_surface);
-//                    }
-//                    lod1_2_geometry["boundaries"] = roof_multi_surface;
-//                    co.value()["geometry"].push_back(lod1_2_geometry);
+
+                                for (size_t k = 0; k < inner_ground_surface.size(); k++) {
+                                    vec<vec<int>> wall_surface;
+                                    vec<int> inner_wall_surface;
+
+                                    int ground_vertex_1 = inner_ground_surface[k];
+
+                                    int ground_vertex_2 = inner_ground_surface[(k + 1) %
+                                                                               inner_ground_surface.size()]; // This is because it should be CCW. In case when it's inner boundary, the orientation should be oppsoite
+
+                                    auto it1 = std::find_if(roof_ground_pairs.begin(),
+                                                            roof_ground_pairs.end(),
+                                                            [ground_vertex_1](const pair<int, int> &pair) {
+                                                                return pair.first == ground_vertex_1;
+                                                            });
+                                    auto it2 = std::find_if(roof_ground_pairs.begin(),
+                                                            roof_ground_pairs.end(),
+                                                            [ground_vertex_2](const pair<int, int> &pair) {
+                                                                return pair.first == ground_vertex_2;
+                                                            });
+                                    inner_wall_surface.push_back(ground_vertex_2);
+                                    inner_wall_surface.push_back(ground_vertex_1);
+                                    inner_wall_surface.push_back(it1->second);
+                                    inner_wall_surface.push_back(it2->second);
+                                    wall_surface.push_back(inner_wall_surface);
+                                    wallsurfaces.push_back(wall_surface);
+                                }
+                            }
+                            wallsurfaces.push_back(roof_multi_surface[i]);
+                            lod1_2_boundaries = wallsurfaces;
+                        }
+                        lod1_2_geometry["boundaries"] = lod1_2_boundaries;
+                        co.value()["geometry"].push_back(lod1_2_geometry);
+
+                    }
                 }
             }
         }
@@ -332,46 +330,7 @@ json extract_lod1_2(json &js) {
     return js;
 }
 
-// The way using volume
-// json extract_lod1_2(json &js) {
-//   const vec<double> scales = js["transform"]["scale"];
-//   const vec<double> translates = js["transform"]["translate"];
-//   for (auto &co : js["CityObjects"].items()) {
-//     if (std::find(BUILDING_TYPE.begin(), BUILDING_TYPE.end(),
-//                   co.value()["type"]) != BUILDING_TYPE.end()) {
-//       json lod1_2_geometry = {{"lod", "1.2"}, {"type", "Solid"}};
-//       for (auto &g : co.value()["geometry"]) {
-//         double lod2_2_volume;
-//         if (g["lod"] == "2.2") {
-//           vec<vec<vec<vec<Point3>>>> shells_v;
-//           for (auto &shell : g["boundaries"]) {
-//             vec<vec<vec<Point3>>> shell_v;
-//             for (auto &surface : shell) {
-//               vec<vec<Point3>> surface_v;
-//               for (auto &ring : surface) {
-//                 vec<Point3> ring_v;
-//                 for (auto &v : ring) {
-//                   vec<int> vi = js["vertices"][v.get<int>()];
-//                   vec<double> tv = transform(vi, scales, translates);
-//                   Point3 point = Point3(tv[0], tv[1], tv[2]);
-//                   ring_v.push_back(point);
-//                 }
-//                 surface_v.push_back(ring_v);
-//               }
-//               Polyhedron3 polyhedron;
-//               PolyhedronBuilder<HalfedgeDS> builder(surface_v);
-//               polyhedron.delegate(builder);
-//               double vol =
-//               CGAL::Polygon_mesh_processing::volume(polyhedron);
-//               shell_v.push_back(surface_v);
-//             }
-//             shells_v.push_back(shell_v);
-//           }
-//         }
-//       }
-//     }
-//   }
-// }
+
 // json extract_ground_surface(json &j) {
 //  std::vector<std::string> buildingType = {"Building", "BuildingPart",
 //                                           "BuildingRoom", "BuildingStorey",
